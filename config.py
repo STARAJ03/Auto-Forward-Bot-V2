3a35665affa3d1c6a56613ebb648c19a130d05b3
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-07-24 12:13:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "9df1f705c8047ac0d723b29069a1332b")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://staraj67890:HvA9VVkHVp7Jzdox@cluster0.jhpzxdh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1116405290').split()]
 
